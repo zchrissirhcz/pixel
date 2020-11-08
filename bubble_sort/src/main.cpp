4b88e659c5cbedc486d232b1051f61541446c807
Zhuo Zhang <imzhuo@foxmail.com>
2020-11-08 19:47:09 +0800

    refactor: [bubble_sort]
    - measure time in cpp code
    - remove global vars

--- bubble_sort/src/main.cpp
+++ bubble_sort/src/main.cpp
@@ -1,13 +1,27 @@
 #include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-#define N 32768
+#include <iostream>
+#include <string>
+#include <chrono>
 
-int data[N];
+using namespace std;
 
-int main() {
+double msElapsedTime(chrono::system_clock::time_point start) {
+    auto end = chrono::system_clock::now();
+
+    return chrono::duration_cast<chrono::milliseconds>(end - start).count();
+}
+
+chrono::system_clock::time_point now() {
+    return chrono::system_clock::now();
+}
+
+void bubble_sort(int* data, int n) {
     int i, j, temp;
-    for (i=0; i<N-1; i++) {
-        for (j=0; j<N-1; j++) {
+    for (i=0; i<n-1; i++) {
+        for (j=0; j<n-1; j++) {
             if (data[j] > data[j+1]) {
                 temp = data[j];
                 data[j] = data[j+1];
@@ -15,7 +29,21 @@ int main() {
             }
         }
     }
-    return 1;
+}
+
+int main()
+{
+    int n = 32768;
+    int* data = (int*)malloc(n*sizeof(int));
+
+    auto t_start = now();
+    bubble_sort(data, n);
+    auto t_cost = msElapsedTime(t_start);
+
+    string res = "naive impl cost " + to_string((int)t_cost) + " ms";
+    cout << res << endl;
+
+    free(data);
 
     return 0;
 }
