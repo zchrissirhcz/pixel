03386d90c7f85cc79aa6184d13f917b1c2fed00b
Zhuo Zhang <imzhuo@foxmail.com>
2020-11-12 19:56:54 +0800

    chore: better build scripts

--- ndk-hello-neon/build/run-android-arm64.cmd
+++ ndk-hello-neon/build/run-android-arm64.cmd
@@ -4,6 +4,3 @@ set DST_DIR="/data/tmp"
 adb shell "mkdir -p %DST_DIR%"
 adb push arm64-v8a/demo %DST_DIR%
 adb shell "cd %DST_DIR%; chmod +x %DST_DIR%/demo; ./demo"
-
-adb pull /data/tmp/neon.jpg ./
-adb pull /data/tmp/non_neon.jpg ./
