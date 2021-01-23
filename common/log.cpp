51ece35e2f737a1e674e414349c85bcf2ad8333e
zhuo@mbp <imzhuo@foxmail.com>
2021-01-23 15:33:52 +0800

    refactor: configurable log util

--- common/log.cpp
+++ common/log.cpp
@@ -89,20 +89,20 @@
 //#define LOGD(fmt, ...) LOGT(fmt, stdout, ##__VA_ARGS__)
 //#define LOGE(fmt, ...) LOGT(fmt, stderr, ##__VA_ARGS__)
 
-#include <time.h>
-static inline char* timenow() {
-    time_t rawtime = time(NULL);
-    struct tm* timeinfo = localtime(&rawtime);
-    static char now[64];
-    // if timezone required, use %z
-    //now[strftime(now, sizeof(now), "%Y-%m-%d %H:%M:%S(%z)", timeinfo)] = '\0';
-    now[strftime(now, sizeof(now), "%Y-%m-%d %H:%M:%S", timeinfo)] = '\0';
-    return now;
-}
-#define DEFAULT_TAG "pixel"
-#define LOGT(level, fmt, tag, fd, color, ...) fprintf(fd, ("%s | %s | %s[%-5s]\x1b[0m [File %s][Line %d] %s" fmt "\n"), timenow(), tag, color, level, __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__)
-#define LOGD(fmt, ...) LOGT("DEBUG", fmt, DEFAULT_TAG, stdout, "\x1b[36m", ##__VA_ARGS__)
-#define LOGE(fmt, ...) LOGT("ERROR", fmt, DEFAULT_TAG, stderr, "\x1b[31m", ##__VA_ARGS__)
+//#include <time.h>
+//static inline char* timenow() {
+//    time_t rawtime = time(NULL);
+//    struct tm* timeinfo = localtime(&rawtime);
+//    static char now[64];
+//    // if timezone required, use %z
+//    //now[strftime(now, sizeof(now), "%Y-%m-%d %H:%M:%S(%z)", timeinfo)] = '\0';
+//    now[strftime(now, sizeof(now), "%Y-%m-%d %H:%M:%S", timeinfo)] = '\0';
+//    return now;
+//}
+//#define DEFAULT_TAG "pixel"
+//#define LOGT(level, fmt, tag, fd, color, ...) fprintf(fd, ("%s | %s | %s[%-5s]\x1b[0m [File %s][Line %d] %s" fmt "\n"), timenow(), tag, color, level, __FILE__, __LINE__, __PRETTY_FUNCTION__, ##__VA_ARGS__)
+//#define LOGD(fmt, ...) LOGT("DEBUG", fmt, DEFAULT_TAG, stdout, "\x1b[36m", ##__VA_ARGS__)
+//#define LOGE(fmt, ...) LOGT("ERROR", fmt, DEFAULT_TAG, stderr, "\x1b[31m", ##__VA_ARGS__)
 
 // 似乎把这么全面格式的宏定义在一行内实现，可维护性并不那么好，考虑参照如下开源项目改进
 // https://github.com/dmcrodrigues/macro-logger/blob/master/macrologger.h
@@ -110,22 +110,263 @@ static inline char* timenow() {
 
 
 
+/*********************************************************************************
+ * [Usage]:
+ *      const char* name = "ChrisZZ";
+ *      PIXEL_LOGD("hello, %s", name);
+ *      PIXEL_LOGI("This is an info message");
+ *      PIXEL_LOGE("I am error !");
+ *      int n = 5;
+ *      PIXEL_LOG_IF_ERROR(n!=0, "n is not zero, this is error");
+ * 
+ * [Customization]:
+ *  0. Search [user setting] and modify, such as:
+ *  1. Turn on/off one log level by modify values of:
+ *      _PXL_LOG_DEBUG_ENABLED
+ *      _PXL_LOG_INFO_ENABLED
+ *      _PXL_LOG_WARN_ENABLED
+ *      _PXL_LOG_ERROR_ENABLED
+ *  2. Select pre-defined log format by define to 1 in **only one** of:
+ *      _PXL_LOGFMT_FULL
+ *      _PXL_LOGFMT_MEDIUM
+ *      _PXL_LOGFMT_SIMPLE
+ *********************************************************************************/
+
+#include <time.h>
+#include <string.h>
+
+#ifdef ANDROID
+#include <android/log.h>
+#endif
+
+// Compatible with Android NDK
+enum PixelLogPriority {
+    PIXEL_LOG_DEBUG = 3,
+    PIXEL_LOG_INFO,
+    PIXEL_LOG_WARN,
+    PIXEL_LOG_ERROR,
+};
+const static char* g_pixel_log_priority_str[7] = {
+    0, 0, 0,
+    "Debug",
+    "Info",
+    "Warn",
+    "Error"
+};
+
+//--------------------------------------------------------------------------------
+// => switches for each log level
+// [user setting]
+//--------------------------------------------------------------------------------
+#define _PXL_LOG_DEBUG_ENABLED      1
+#define _PXL_LOG_INFO_ENABLED       1
+#define _PXL_LOG_WARN_ENABLED       1
+#define _PXL_LOG_ERROR_ENABLED      1
+
+//--------------------------------------------------------------------------------
+// => select log format
+// [user setting]
+//--------------------------------------------------------------------------------
+#define _PXL_LOGFMT_FULL            1
+#define _PXL_LOGFMT_MEDIUM          0
+#define _PXL_LOGFMT_SIMPLE          0
+
+//--------------------------------------------------------------------------------
+// => filename
+//--------------------------------------------------------------------------------
+// only filename
+#define _PXL_FILE strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__
+// contain folder name such as src/log.cpp
+//#define _FILE __FILE__
+
+//--------------------------------------------------------------------------------
+// => function
+//--------------------------------------------------------------------------------
+// only function name
+#define _PXL_FUNCTION __FUNCTION__
+// function name with parameter types
+//#define _FUNCTION __PRETTY_FUNCTION__
+
+//--------------------------------------------------------------------------------
+// => tag
+//--------------------------------------------------------------------------------
+// the tag for current module/library/file
+#define _PXL_MODULE_TAG "pixel"
+
+
+#if _PXL_LOGFMT_FULL
+static inline char* timenow() {
+    time_t rawtime = time(NULL);
+    struct tm* timeinfo = localtime(&rawtime);
+    static char now[64];
+
+//--------------------------------------------------------------------------------
+// => timestamp format
+//--------------------------------------------------------------------------------
+#define _PXL_TIMESTAMP_FMT "%Y-%m-%d %H:%M:%S"
+// if timezone required, use %z
+//#define TIMESTAMP_FORMAT "%Y-%m-%d %H:%M:%S(%z)"
+
+    now[strftime(now, sizeof(now), _PXL_TIMESTAMP_FMT, timeinfo)] = '\0';
+#undef _PXL_TIMESTAMP_FMT
+    return now;
+}
+#endif
+
+//--------------------------------------------------------------------------------
+// => log format
+//--------------------------------------------------------------------------------
+// the param `fmt` is user specified format
+// _PXL_LOG_FULL_FMT(fmt) ammed pre-defined stuffs (time, filename, line num, function) with colors
+#define _PXL_LOG_FULL_FMT(fmt)                   ("%s | %s | %s[%-5s]\x1b[0m | %s, line %d, %s | " fmt "\n")
+#define _PXL_LOG_FULL_ARGS(tag, color, priority) timenow(), tag, color, g_pixel_log_priority_str[priority], _PXL_FILE, __LINE__, _PXL_FUNCTION
+
+// the param `fmt` is user specified format
+// _PXL_LOG_MEDIUM_FMT(fmt) ammed pre-defined stuffs (filename, line num)
+#define _PXL_LOG_MEDIUM_FMT(fmt)                   ("%s/%s%-5s\x1b[0m | %s, line %d | " fmt "\n")
+#define _PXL_LOG_MEDIUM_ARGS(tag, color, priority) tag, color, g_pixel_log_priority_str[priority], _PXL_FILE, __LINE__
+
+
+//--------------------------------------------------------------------------------
+// => log template for all levels
+//--------------------------------------------------------------------------------
+// the full format log
+#ifdef ANDROID
+#define _PXL_LOGT_FULL(priority, fmt, tag, fd, color, ...) do { \
+        __android_log_print(priority, tag, _PXL_LOG_FULL_FMT(fmt), _PXL_LOG_FULL_ARGS(tag, color, priority), ##__VA_ARGS__); \
+        fprintf(fd, _PXL_LOG_FULL_FMT(fmt), _PXL_LOG_FULL_ARGS(tag, color, priority), ##__VA_ARGS__); \
+    } while(0)
+#else
+#define _PXL_LOGT_FULL(priority, fmt, tag, fd, color, ...) do { \
+        fprintf(fd, _PXL_LOG_FULL_FMT(fmt), _PXL_LOG_FULL_ARGS(tag, color, priority), ##__VA_ARGS__); \
+    } while(0)
+#endif
+
+// the medium format log
+#ifdef ANDROID
+#define _PXL_LOGT_MEDIUM(priority, fmt, tag, fd, color, ...) do { \
+        __android_log_print(priority, tag, _PXL_LOG_MEDIUM_FMT(fmt), _PXL_LOG_MEDIUM_ARGS(tag, color, priority), ##__VA_ARGS__); \
+        fprintf(fd, _PXL_LOG_MEDIUM_FMT(fmt), _PXL_LOG_MEDIUM_ARGS(tag, color, priority), ##__VA_ARGS__); \
+    } while(0)
+#else
+#define _PXL_LOGT_MEDIUM(priority, fmt, tag, fd, color, ...) do { \
+        fprintf(fd, _PXL_LOG_MEDIUM_FMT(fmt), _PXL_LOG_MEDIUM_ARGS(tag, color, priority), ##__VA_ARGS__); \
+    } while(0)
+#endif
+
+// simple format log
+#ifdef ANDROID
+#define _PXL_LOGT_SIMPLE(priority, fmt, ...) do { \
+       __android_log_print(priority, _PXL_MODULE_TAG, fmt, ##__VA_ARGS__); \
+       fprintf(stdout, (fmt "\n"), ##__VA_ARGS__); \
+    } while(0)
+#else
+#define _PXL_LOGT_SIMPLE(priority, fmt, ...) do { \
+        fprintf(stdout, (fmt "\n"), ##__VA_ARGS__); \
+    } while(0)
+#endif
+
+
+//--------------------------------------------------------------------------------
+// => log template for each level
+//--------------------------------------------------------------------------------
+// NOTE: if using stderr, `run-and-install.sh`(start on PC, run on Android) will mess up log order
+// For better log order, always use stdout
+#if _PXL_LOGFMT_FULL
+#define _PXL_LOGDT_FULL(fmt, ...) _PXL_LOGT_FULL(PIXEL_LOG_DEBUG, fmt, _PXL_MODULE_TAG, stdout, "\x1b[36m", ##__VA_ARGS__)
+#define _PXL_LOGIT_FULL(fmt, ...) _PXL_LOGT_FULL(PIXEL_LOG_INFO,  fmt, _PXL_MODULE_TAG, stdout, "\x1b[32m", ##__VA_ARGS__)
+#define _PXL_LOGWT_FULL(fmt, ...) _PXL_LOGT_FULL(PIXEL_LOG_WARN,  fmt, _PXL_MODULE_TAG, stdout, "\x1b[33m", ##__VA_ARGS__)
+#define _PXL_LOGET_FULL(fmt, ...) _PXL_LOGT_FULL(PIXEL_LOG_ERROR, fmt, _PXL_MODULE_TAG, stdout, "\x1b[31m", ##__VA_ARGS__)
+#elif _PXL_LOGFMT_MEDIUM
+#define _PXL_LOGDT_MEDIUM(fmt, ...) _PXL_LOGT_MEDIUM(PIXEL_LOG_DEBUG, fmt, _PXL_MODULE_TAG, stdout, "\x1b[36m", ##__VA_ARGS__)
+#define _PXL_LOGIT_MEDIUM(fmt, ...) _PXL_LOGT_MEDIUM(PIXEL_LOG_INFO,  fmt, _PXL_MODULE_TAG, stdout, "\x1b[32m", ##__VA_ARGS__)
+#define _PXL_LOGWT_MEDIUM(fmt, ...) _PXL_LOGT_MEDIUM(PIXEL_LOG_WARN,  fmt, _PXL_MODULE_TAG, stdout, "\x1b[33m", ##__VA_ARGS__)
+#define _PXL_LOGET_MEDIUM(fmt, ...) _PXL_LOGT_MEDIUM(PIXEL_LOG_ERROR, fmt, _PXL_MODULE_TAG, stdout, "\x1b[31m", ##__VA_ARGS__)
+#elif _PXL_LOGFMT_SIMPLE
+#define _PXL_LOGDT_SIMPLE(fmt, ...) _PXL_LOGT_SIMPLE(PIXEL_LOG_DEBUG, fmt, ##__VA_ARGS__)
+#define _PXL_LOGIT_SIMPLE(fmt, ...) _PXL_LOGT_SIMPLE(PIXEL_LOG_INFO,  fmt, ##__VA_ARGS__)
+#define _PXL_LOGWT_SIMPLE(fmt, ...) _PXL_LOGT_SIMPLE(PIXEL_LOG_WARN,  fmt, ##__VA_ARGS__)
+#define _PXL_LOGET_SIMPLE(fmt, ...) _PXL_LOGT_SIMPLE(PIXEL_LOG_ERROR, fmt, ##__VA_ARGS__)
+#else
+#pragma message("Please define one of _PXL_LOGFMT_FULL, _PXL_LOGFMT_MEDIUM or _PXL_LOGFMT_SIMPLE") 
+#endif
+
+//--------------------------------------------------------------------------------
+// => specify each log level's template
+// [user setting]
+//--------------------------------------------------------------------------------
+// use full format template
+#if _PXL_LOGFMT_FULL
+#define _PXL_LOGDT(fmt, ...) _PXL_LOGDT_FULL(fmt, ##__VA_ARGS__)
+#define _PXL_LOGIT(fmt, ...) _PXL_LOGIT_FULL(fmt, ##__VA_ARGS__)
+#define _PXL_LOGWT(fmt, ...) _PXL_LOGWT_FULL(fmt, ##__VA_ARGS__)
+#define _PXL_LOGET(fmt, ...) _PXL_LOGET_FULL(fmt, ##__VA_ARGS__)
+#elif _PXL_LOGFMT_MEDIUM
+#define _PXL_LOGDT(fmt, ...) _PXL_LOGDT_MEDIUM(fmt, ##__VA_ARGS__)
+#define _PXL_LOGIT(fmt, ...) _PXL_LOGIT_MEDIUM(fmt, ##__VA_ARGS__)
+#define _PXL_LOGWT(fmt, ...) _PXL_LOGWT_MEDIUM(fmt, ##__VA_ARGS__)
+#define _PXL_LOGET(fmt, ...) _PXL_LOGET_MEDIUM(fmt, ##__VA_ARGS__)
+#elif _PXL_LOGFMT_SIMPLE
+#define _PXL_LOGIT(fmt, ...) _PXL_LOGIT_SIMPLE(fmt, ##__VA_ARGS__)
+#define _PXL_LOGDT(fmt, ...) _PXL_LOGDT_SIMPLE(fmt, ##__VA_ARGS__)
+#define _PXL_LOGWT(fmt, ...) _PXL_LOGWT_SIMPLE(fmt, ##__VA_ARGS__)
+#define _PXL_LOGET(fmt, ...) _PXL_LOGET_SIMPLE(fmt, ##__VA_ARGS__)
+#else
+#pragma message("Please define one of _PXL_LOGFMT_FULL, _PXL_LOGFMT_MEDIUM or _PXL_LOGFMT_SIMPLE") 
+#endif
+
+//--------------------------------------------------------------------------------
+// => enable log macro according to LOG_LEVEL
+//--------------------------------------------------------------------------------
+#if _PXL_LOG_INFO_ENABLED
+#define PIXEL_LOGI(fmt, ...) _PXL_LOGIT(fmt, ##__VA_ARGS__)
+#else
+#define PIXEL_LOGI(fmt, ...)
+#endif
+
+#if _PXL_LOG_DEBUG_ENABLED
+#define PIXEL_LOGD(fmt, ...) _PXL_LOGDT(fmt, ##__VA_ARGS__)
+#else
+#define PIXEL_LOGD(fmt, ...)
+#endif
+
+#if _PXL_LOG_WARN_ENABLED
+#define PIXEL_LOGW(fmt, ...) _PXL_LOGWT(fmt, ##__VA_ARGS__)
+#else
+#define PIXEL_LOGW(fmt, ...)
+#endif
+
+#if _PXL_LOG_ERROR_ENABLED
+#define PIXEL_LOGE(fmt, ...) _PXL_LOGET(fmt, ##__VA_ARGS__)
+#else
+#define PIXEL_LOGE(fmt, ...)
+#endif
+
+#if _PXL_LOG_ERROR_ENABLED
+#define PIXEL_LOG_IF_ERROR(condition, fmt, ...) do { \
+        if (condition) PIXEL_LOGE(fmt, ##__VA_ARGS__); \
+    } while(0) 
+#else
+#define PIXEL_LOG_IF_ERROR(condition, fmt, ...)
+#endif
+
 void hello(const char* name) {
-    LOGD("hellol, %s", name);
+    PIXEL_LOGD("hello, %s", name);
 }
 
 int main() {
 
-    LOGD("hello world %d", 3);
+    PIXEL_LOGD("hello world %d", 3);
     const char* str = "hello";
     int n = 10;
-    LOGD("num %d, str %s", n, str);
-    LOGD("%d", 3);
+    PIXEL_LOGD("num %d, str %s", n, str);
+    PIXEL_LOGD("%d", 3);
     hello("ChrisZZ");
-    LOGE("this is an error message");
+    PIXEL_LOGE("this is an error message");
+    PIXEL_LOGI("this is an info message");
+
+    n=3;
+    PIXEL_LOG_IF_ERROR(n!=0, "n is not zero");
 
     return 0;
 }
-
-
-
