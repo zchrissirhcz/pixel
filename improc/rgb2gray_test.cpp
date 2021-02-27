54672c1d27f2ce4b6653c4c72f3fa861455f794c
Zhuo Zhang <imzhuo@foxmail.com>
2021-02-27 15:44:24 +0800

    chore: remove useless calls

--- improc/rgb2gray_test.cpp
+++ improc/rgb2gray_test.cpp
@@ -69,22 +69,5 @@ int main() {
     PIXEL_LOGD("rgb2gray, fixed asm, size h=%zu, w=%zu, time cost %.6lf ms\n", height, width, t_cost);
     cv::imwrite("sky_gray_fixed_asm.bmp", gray_fixed_asm);
 
-#if 0
-    cv::Mat gray_mean(size, CV_8UC1);
-    gray_buf = gray_mean.data;
-    f_rgb_to_gray(rgb_buf, height, width, rgb_linebytes, gray_buf, gray_linebytes, 0);
-    cv::imwrite("mingren_gray_mean.bmp", gray);
-
-    cv::Mat gray_classical(size, CV_8UC1);
-    gray_buf = gray_classical.data;
-    f_rgb_to_gray(rgb_buf, height, width, rgb_linebytes, gray_buf, gray_linebytes, 0);
-    cv::imwrite("mingren_gray_classical.bmp", gray);
-
-    cv::Mat gray_photoshop(size, CV_8UC1);
-    gray_buf = gray_photoshop.data;
-    f_rgb_to_gray(rgb_buf, height, width, rgb_linebytes, gray_buf, gray_linebytes, 0);
-    cv::imwrite("mingren_gray_photoshop.bmp", gray);
-#endif
-
     return 0;
 }
