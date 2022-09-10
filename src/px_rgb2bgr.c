7888227f745ac473e2c3d03191e427862c27609d
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-10 18:07:55 +0800

    refactor: rgb2gray, rgb2bgr: inner loop remove dependency of loop var

--- src/px_rgb2bgr.c
+++ src/px_rgb2bgr.c
@@ -10,16 +10,17 @@ void px_rgb2bgr(px_image_t* src, px_image_t* dst)
     const int h = src->height;
     const int w = src->width;
     const int cn = src->channel;
-    const int rowlen = w * cn;
     for (int i = 0; i < h; i++)
     {
         uint8_t* sp = src->data + i * src->stride;
         uint8_t* dp = dst->data + i * dst->stride;
-        for (int j = 0; j < rowlen; j += cn)
+        for (int j = 0; j < w; j++)
         {
-            dp[j + 0] = sp[j + 2];
-            dp[j + 1] = sp[j + 1];
-            dp[j + 2] = sp[j + 0];
+            dp[0] = sp[2];
+            dp[1] = sp[1];
+            dp[2] = sp[0];
+            dp += cn;
+            sp += cn;
         }
     }
 }
--- src/px_rgb2gray.c
+++ src/px_rgb2gray.c
@@ -11,18 +11,18 @@ void px_rgb2gray(px_image_t* src, px_image_t* dst)
     const int h = src->height;
     const int w = src->width;
     const int cn = src->channel;
-    const int rowlen = cn * w;
     for (int i = 0; i < h; i++)
     {
         uint8_t* sp = src->data + i * src->stride;
         uint8_t* dp = dst->data + i * dst->stride;
-        for (int j = 0, dj = 0; j < rowlen; j+=cn, dj++)
+        for (int j = 0; j < w; j++)
         {
-            float r = sp[j + 0];
-            float g = sp[j + 1];
-            float b = sp[j + 2];
-            float gray = (0.299f * r + 0.587f * g + 0.114f * b) + 0.5f;
-            dp[dj] = gray;
+            double r = sp[0];
+            double g = sp[1];
+            double b = sp[2];
+            double gray = (0.299 * r + 0.587 * g + 0.114 * b) + 0.5;
+            *dp++ = gray;
+            sp += cn;
         }
     }
 
