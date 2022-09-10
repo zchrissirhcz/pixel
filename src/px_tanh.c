df3831707e90696ab2ee17ded405e90e1eb6f1c8
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-10 19:35:28 +0800

    fix: px_tanh.c to build and fix calling

--- src/px_tanh.c
+++ src/px_tanh.c
@@ -6,7 +6,7 @@ void px_forward_tanh_layer_for_matrix(const px_matrix_t* input, px_matrix_t* out
     return px_forward_eltwise_layer_for_matrix(input, output, tanhf);
 }
 
-px_cube_t* px_forward_tanh_layer_for_cube(const px_cube_t* input)
+void px_forward_tanh_layer_for_cube(const px_cube_t* input, px_cube_t* output)
 {
-    return px_forward_eltwise_layer_for_cube(input, tanhf);
+    return px_forward_eltwise_layer_for_cube(input, output, tanhf);
 }
