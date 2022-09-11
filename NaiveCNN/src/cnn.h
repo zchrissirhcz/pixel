7bb9b25a662a0d97ff5abf3ad2dfbd492cfe6b02
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-11 09:41:46 +0800

    clean: cnn.h indentation adjusted

--- NaiveCNN/src/cnn.h
+++ NaiveCNN/src/cnn.h
@@ -39,7 +39,7 @@ typedef struct ConvLayer{
 
     // 输出像素的局部梯度
     float*** d; // 网络的局部梯度,δ值
-}ConvLayer;
+} ConvLayer;
 
 // 池化层 pooling
 typedef struct PoolingLayer{
@@ -55,7 +55,7 @@ typedef struct PoolingLayer{
 
     float*** y; // 采样函数后神经元的输出,无激活函数
     float*** d; // 网络的局部梯度,δ值
-}PoolingLayer;
+} PoolingLayer;
 
 // 输出层 全连接的神经网络
 typedef struct InnerproductLayer{
@@ -71,7 +71,7 @@ typedef struct InnerproductLayer{
     float* d; // 网络的局部梯度,δ值
 
     bool isFullConnect; //是否为全连接
-}InnerproductLayer;
+} InnerproductLayer;
 
 typedef struct cnn_network{
     int layerNum;
@@ -83,63 +83,60 @@ typedef struct cnn_network{
 
     float* e; // 训练误差
     float* L; // 瞬时误差能量
-}CNN;
+} CNN;
 
 typedef struct train_opts{
     int numepochs; // 训练的迭代次数
     float alpha; // 学习速率
-}CNNOpts;
+} CNNOpts;
 
 
-//---------------------------------------------------------------------
-// function declarations
-//---------------------------------------------------------------------
 #ifdef __cplusplus
 extern "C" {
 #endif
 
-    // 保存cnn
-    void save_cnn(CNN* cnn, const char* filename);
+// 保存cnn
+void save_cnn(CNN* cnn, const char* filename);
 
-    // 导入cnn的数据
-    void load_cnn(CNN* cnn, const char* filename);
+// 导入cnn的数据
+void load_cnn(CNN* cnn, const char* filename);
 
-    // 初始化卷积层
-    ConvLayer* init_conv_layer(int in_width, int in_height, int map_size, int in_channels, int out_channels);
+// 初始化卷积层
+ConvLayer* init_conv_layer(int in_width, int in_height, int map_size, int in_channels, int out_channels);
 
-    // 初始化采样层
-    PoolingLayer* init_pooling_layer(int in_width, int in_height, int map_size, int in_channels, int out_channels, int pool_type);
+// 初始化采样层
+PoolingLayer* init_pooling_layer(int in_width, int in_height, int map_size, int in_channels, int out_channels, int pool_type);
 
-    // 初始化输出层
-    InnerproductLayer* init_innerproduct_layer(int in_num, int out_num);
+// 初始化输出层
+InnerproductLayer* init_innerproduct_layer(int in_num, int out_num);
 
-    // 激活函数 input是数据，inputNum说明数据数目，bas表明偏置
-    float activation_sigma(float input, float bias); // sigma激活函数
+// 激活函数 input是数据，inputNum说明数据数目，bas表明偏置
+float activation_sigma(float input, float bias); // sigma激活函数
 
-    void cnn_forward(CNN* cnn, float** input_data); // 网络的前向传播
+void cnn_forward(CNN* cnn, float** input_data); // 网络的前向传播
 
-    void cnn_backward(CNN* cnn, float* output_data); // 网络的后向传播
+void cnn_backward(CNN* cnn, float* output_data); // 网络的后向传播
 
-    void cnn_applygrads(CNN* cnn, CNNOpts opts, float** input_data);
+void cnn_applygrads(CNN* cnn, CNNOpts opts, float** input_data);
 
-    void cnn_clear(CNN* cnn); // 将数据vyd清零
+void cnn_clear(CNN* cnn); // 将数据vyd清零
 
-    /*
-        Pooling Function
-        input 输入数据
-        inputNum 输入数据数目
-        mapSize 求平均的模块区域
-    */
-    void avg_pooling(float** out, NcSize2D out_size, float** in, NcSize2D in_size, int map_size); // 求平均值
+/*
+    Pooling Function
+    input 输入数据
+    inputNum 输入数据数目
+    mapSize 求平均的模块区域
+*/
+void avg_pooling(float** out, NcSize2D out_size, float** in, NcSize2D in_size, int map_size); // 求平均值
 
-    // 单层全连接神经网络的前向传播
-    void nnff(float* output, float* input, float** wdata, float* bias, NcSize2D nn_size);
+// 单层全连接神经网络的前向传播
+void nnff(float* output, float* input, float** wdata, float* bias, NcSize2D nn_size);
 
-    // 保存CNN网络中的相关数据
-    void save_cnndata(CNN* cnn, const char* filename, float** inputdata);
+// 保存CNN网络中的相关数据
+void save_cnndata(CNN* cnn, const char* filename, float** inputdata);
 
-    // 返回向量最大数的序号
-    int vecmax_index(float* vec, int veclength);
+// 返回向量最大数的序号
+int vecmax_index(float* vec, int veclength);
 
 #ifdef __cplusplus
 }
