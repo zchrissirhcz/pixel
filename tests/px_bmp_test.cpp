3213aec2452276fc963895f797ff7ca79a18a89f
Zhuo Zhang <imzhuo@foxmail.com>
2022-09-14 08:57:16 +0800

    fix: correctly use bpp in px_bmp_test.cpp

--- tests/px_bmp_test.cpp
+++ tests/px_bmp_test.cpp
@@ -28,7 +28,7 @@ void test_read_image_no_custom(const char* filename)
 
 TEST(read_bmp, no_align)
 {
-    // bpp=3, BGR image, not aligned
+    // bpp=24, BGR image, not aligned
     const char* bgr_image_path = "mingren.bmp"; //width=450, not multiple of 4
     test_read_image_no_custom(bgr_image_path);
 
@@ -37,14 +37,13 @@ TEST(read_bmp, no_align)
     test_read_image_no_custom(gray_image_path);
 }
 
-void test_read_bmp_with_custom(const char* filename)
+void test_read_bmp_with_custom(const char* filename, int align, bool swap_color)
 {
     unsigned char* buffer = NULL;
     int height;
     int width;
     int channel;
-    int align = 4;
-    px_read_bmp_custom(filename, &height, &width, &channel, &buffer, align, false);
+    px_read_bmp_custom(filename, &height, &width, &channel, &buffer, align, swap_color);
     
     cv::Size size;
     size.height = height;
@@ -79,20 +78,22 @@ void test_read_bmp_with_custom(const char* filename)
 
 TEST(read_bmp, align)
 {
-    // bpp=3, BGR image, aligned
+    const int align = 4;
+    const bool swap_color = false;
+
+    // bpp=24, BGR image, aligned
     const char* bgr_image_path = "mingren.bmp";
-    test_read_bmp_with_custom(bgr_image_path);
+    test_read_bmp_with_custom(bgr_image_path, align, swap_color);
 
     // gray image (bpp=8, using palette to fill color), align = 4
     const char* gray_image_path = "mingren_gray.bmp";
-    test_read_bmp_with_custom(gray_image_path);
+    test_read_bmp_with_custom(gray_image_path, align, swap_color);
 }
 
 void test_write_bmp_no_custom(const char* filepath)
 {
     cv::Mat mat = cv::imread(filepath, cv::IMREAD_UNCHANGED);
     unsigned char* buffer = mat.data;
-    int linebytes = mat.step[0];
     cv::Size size = mat.size();
     int height = size.height;
     int width = size.width;
@@ -110,7 +111,7 @@ void test_write_bmp_no_custom(const char* filepath)
 
 TEST(write_bmp, no_custom)
 {
-    // bpp=3, no align, save image
+    // bpp=24, no align, save image
     const char* bgr_image_path = "mingren.bmp";
     test_write_bmp_no_custom(bgr_image_path);
 
@@ -136,7 +137,7 @@ TEST(write_bmp, custom_align)
     const int align = 4;
     const bool swap_color = false;
 
-    // bpp=3, align, save image
+    // bpp=24, align, save image
     const char* bgr_image_path = "mingren.bmp";
     test_write_bmp_custom(bgr_image_path, align, swap_color);
 
@@ -150,7 +151,7 @@ TEST(write_bmp, custom_color_order)
     const int align = 1;
     const bool swap_color = true;
 
-    // bpp=3, no align, save image, rgb/bgr swap
+    // bpp=24, no align, save image, rgb/bgr swap
     const char* bgr_image_path = "mingren.bmp";
     test_write_bmp_custom(bgr_image_path, align, swap_color);
 }
@@ -160,7 +161,7 @@ TEST(write_bmp, custom_align_and_color_order)
     const int align = 4;
     const bool swap_color = true;
 
-    // bpp=3, align, save image, rgb/bgr swap
+    // bpp=24, align, save image, rgb/bgr swap
     const char* bgr_image_path = "mingren.bmp";
     test_write_bmp_custom(bgr_image_path, align, swap_color);
 
@@ -226,4 +227,19 @@ TEST(read_bmp, gray)
     EXPECT_EQ(width, src.cols);
 
     free(buffer);
+}
+
+
+int main(int argc, char* argv[])
+{
+    testing::InitGoogleTest(&argc, argv);
+
+    // Run a specific test only
+    //testing::GTEST_FLAG(filter) = "MyLibrary.TestReading"; // I'm testing a new feature, run something quickly
+    testing::GTEST_FLAG(filter) = "read_bmp.no_align";
+
+    // Exclude a specific test
+    //testing::GTEST_FLAG(filter) = "-cvtColorTwoPlane.yuv420sp_to_rgb:-cvtColorTwoPlane.rgb_to_yuv420sp"; // The writing test is broken, so skip it
+
+    return RUN_ALL_TESTS();
 }
