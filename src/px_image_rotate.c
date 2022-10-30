955ddefc3609547ab348381ee0bc1e79e472e570
Zhuo Zhang <imzhuo@foxmail.com>
2022-10-30 18:31:18 +0800

    feat: add `px_image_rotate270()`

--- src/px_image_rotate.c
+++ src/px_image_rotate.c
@@ -26,5 +26,22 @@ void px_image_rotate90(px_image_t* src, px_image_t* dst)
 
 void px_image_rotate270(px_image_t* src, px_image_t* dst)
 {
+    PX_ASSERT(dst->width == src->height);
+    PX_ASSERT(dst->height == src->width);
+    PX_ASSERT(dst->channel == src->channel);
 
+    const int channel = src->channel;
+
+    for (int i = 0; i < src->height; i++)
+    {
+        for (int j = 0; j < src->width; j++)
+        {
+            for (int k = 0; k < channel; k++)
+            {
+                const int src_idx = i * src->stride + j * channel + k;
+                const int dst_idx = (dst->height - 1 - j) * dst->stride + i * channel + k;
+                dst->data[dst_idx] = src->data[src_idx];
+            }
+        }
+    }
 }
--- tests/px_image_rotate_test.cpp
+++ tests/px_image_rotate_test.cpp
@@ -40,7 +40,7 @@ TEST(image_rotate90, 1x2_c3)
     const int total = src_height * src_width * channel;
     for (int i = 0; i < total; i++)
     {
-        src->data[i] = i;
+        src->data[i] = i + 1;
     }
 
     const int dst_height = src_width;
@@ -51,7 +51,7 @@ TEST(image_rotate90, 1x2_c3)
 
     for (int i = 0; i < total; i++)
     {
-        EXPECT_TRUE(dst->data[i] == i);
+        EXPECT_TRUE(dst->data[i] == i + 1);
     }
 }
 
@@ -133,6 +133,128 @@ TEST(image_rotate90, 2x2_c3)
     uint8_t expected[] = {
         31, 32, 33,  11, 12, 13,
         41, 42, 43,  21, 22, 23
+    };
+
+    EXPECT_TRUE(array_equal_u8(expected, dst->data, total));
+}
+
+
+TEST(image_rotate270, 1x2_c1)
+{
+    //
+    // 1 2    =>   2
+    //             1
+
+    const int src_height = 1;
+    const int src_width = 2;
+    const int channel = 1;
+    px_image_t* src = px_create_image(src_height, src_width, channel);
+    src->data[0] = 1;
+    src->data[1] = 2;
+
+    const int dst_height = src_width;
+    const int dst_width = src_height;
+    px_image_t* dst = px_create_image(dst_height, dst_width, channel);
+
+    px_image_rotate270(src, dst);
+    const int total = src_height * src_width * channel;
+
+    uint8_t expected[] = {
+        2, 1
+    };
+
+    EXPECT_TRUE(array_equal_u8(expected, dst->data, total));
+}
+
+TEST(image_rotate270, 1x2_c3)
+{
+    //
+    // 1 2 3 4 5 6    =>   4 5 6
+    //                     1 2 3
+
+    const int src_height = 1;
+    const int src_width = 2;
+    const int channel = 3;
+    px_image_t* src = px_create_image(src_height, src_width, channel);
+    const int total = src_height * src_width * channel;
+    for (int i = 0; i < total; i++)
+    {
+        src->data[i] = i + 1;
+    }
+
+    const int dst_height = src_width;
+    const int dst_width = src_height;
+    px_image_t* dst = px_create_image(dst_height, dst_width, channel);
+
+    px_image_rotate270(src, dst);
+    uint8_t expected[] = {
+        4, 5, 6,
+        1, 2, 3
+    };
+
+    EXPECT_TRUE(array_equal_u8(expected, dst->data, total));
+}
+
+TEST(image_rotate270, 2x2_c1)
+{
+    //
+    // 1 2    =>   2 4
+    // 3 4         1 3
+
+    const int src_height = 2;
+    const int src_width = 2;
+    const int channel = 1;
+    px_image_t* src = px_create_image(src_height, src_width, channel);
+    const int total = src_height * src_width * channel;
+    for (int i = 0; i < total; i++)
+    {
+        src->data[i] = i + 1;
+    }
+
+    const int dst_height = src_width;
+    const int dst_width = src_height;
+    px_image_t* dst = px_create_image(dst_height, dst_width, channel);
+
+    px_image_rotate270(src, dst);
+
+    uint8_t expected[] = {
+        2, 4,
+        1, 3
+    };
+
+    EXPECT_TRUE(array_equal_u8(expected, dst->data, total));
+}
+
+TEST(image_rotate270, 2x2_c3)
+{
+    //
+    // 11 12 13  21 22 23   =>   21 22 23  41 42 43
+    // 31 32 33  41 42 43        11 12 13  31 32 33
+
+    const int src_height = 2;
+    const int src_width = 2;
+    const int channel = 3;
+    px_image_t* src = px_create_image(src_height, src_width, channel);
+    const int total = src_height * src_width * channel;
+
+    uint8_t input[] = {
+        11, 12, 13,  21, 22, 23,
+        31, 32, 33,  41, 42, 43
+    };
+    for (int i = 0; i < total; i++)
+    {
+        src->data[i] = input[i];
+    }
+
+    const int dst_height = src_width;
+    const int dst_width = src_height;
+    px_image_t* dst = px_create_image(dst_height, dst_width, channel);
+
+    px_image_rotate270(src, dst);
+
+    uint8_t expected[] = {
+        21, 22, 23,  41, 42, 43,
+        11, 12, 13,  31, 32, 33
     };
 
     EXPECT_TRUE(array_equal_u8(expected, dst->data, total));
