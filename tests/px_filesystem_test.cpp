9690318b46b11d94917dddaa9e27e36c2300b38a
Zhuo Zhang <imzhuo@foxmail.com>
2022-11-05 09:43:14 +0800

    test: Add unittests for px_filesystem.h

---
+++ tests/px_filesystem_test.cpp
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+#include "px_compare.h"
+#include "px_filesystem.h"
+
+TEST(filesystem, is_file_exist)
+{
+#if __APPLE__ || __ANDROID__ || __linux__
+    bool state = px_is_file_exist("/tmp");
+#elif __MSC_VER
+    bool state = px_is_file_exist("C:/Windows/system32/etc/driver/hosts")
+#else
+    #pragma message("TODO: add here")
+#endif
+    EXPECT_TRUE(state);
+}
+
+TEST(filesystem, get_pwd)
+{
+    char* pwd = px_getpwd();
+    EXPECT_TRUE(strlen(pwd) > 0);
+}
+
+TEST(filesystem, file_not_exist)
+{
+    PX_FILE_STATE state = px_get_file_or_directory_state("/home/zz/123.txt");
+    EXPECT_TRUE(state == PX_FILE_NOT_EXIST);
+}
